92c28293e8e51dcac354a2655f57cbee7130a8f5
Sébastien Nicolet <snicolet@student.42.fr>
2016-01-23 13:09:53 +0100

    push pour debug le fait que la procedure beaupere_de ne marche pas

--- prolog/step1.pl
+++ prolog/step1.pl
@@ -1,18 +1,31 @@
+/* hommes et femmes */
 homme(X) :- member(X, [marc,jules,jean,luc,leon,loic,gerard,jacques,herve,paul]).
 femme(X) :- member(X, [anne,betty,sylvie,lisa,julie,valerie,eve]).
 
+/* epoux */
 mari_de(X,Y) :- member((X,Y), [(anne,marc),(betty,luc),(lisa,jules),(eve,loic),(paul,julie)]).
 femme_de(X,Y) :- mari_de(Y,X).
 
+/* enfants de marc et anne */
 enfant_de(X,Y) :- member((X,Y), [(marc,jules),(marc,leon),(marc,jean)]).
 enfant_de(X,Y) :- member((X,Y), [(anne,jules),(anne,leon),(anne,jean)]).
-enfant_de(X,Y) :- member((X,Y), [(jules,jacques),(jules,lisa)]).
+
+/* enfants de jules et lisa */
+enfant_de(X,Y) :- member((X,Y), [(jules,jacques),(lisa,jacques)]).
 enfant_de(X,Y) :- member((X,Y), [(leon,herve),(sylvie,herve)]).
 
+/* enfants de leon et sylvie */
+enfant_de(X,Y) :- member((X,Y), [(leon,herve),(leon,julie),(sylvie,herve),(sylvie,julie)]).
 
+/* enfants de loic et eve */
+enfant_de(X,Y) :- member((X,Y), [(loic,paul),(loic,valerie)]).
+enfant_de(X,Y) :- member((X,Y), [(eve,paul),(eve,valerie)]).
+
+/* enfants de luc et betty */
 enfant_de(X,Y) :- member((X,Y), [(luc,gerard),(betty,gerard)]).
 enfant_de(X,Y) :- member((X,Y), [(luc,loic),(betty,loic)]).
 enfant_de(X,Y) :- member((X,Y), [(luc,lisa),(betty,lisa)]).
-enfant_de(X,Y) :- member((X,Y), [(jules,jacques),(lisa,jacques)]).
 
-ancetre_de(X,Y) :- enfant_de(Y,X).
+/* belle famille */
+bellemere_de(X,Y) :- member((X,Y), [(anne,lisa),(anne,sylvie),(sylvie,paul),(betty,eve),(eve,julie)]).
+beaupere_de(X,Y) :- member((X,Y), [(marc,lisa),(marc,sylvie),(leon,paul),(luc,eve),(loic,julie)]).
