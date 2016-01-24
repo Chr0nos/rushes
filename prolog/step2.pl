66b07be18b8b01a57889cbee19baad98b947c9cb
Chr0nos <snicolet@student.42.fr>
2016-01-24 16:05:56 +0100

    passage sur le mba

--- prolog/step2.pl
+++ prolog/step2.pl
@@ -1,11 +1,29 @@
-/* lieux, nationalite, cigar, boisson, animal, couleur, a cote de
+/* lieux, nationalite, cigar, boisson, animal, couleur
 */
-maisons([1, 2, 3, 4, 5],
-	[anglais, suedois, danois, norvegien, allemand],
-	[pallmall, dunhill, bluemaster, princes, blend],
-	[the, cafe, lait, biere, eau],
-	[chiens, oiseaux, poisson, cheval, chats],
-	[rouge, verte, jaune, bleue, blanche]).
+
+agauche_de(A,B) :- A is (B - 1).
+acote_de(A,B) :- agauche_de(A,B).
+acote_de(A,B) :- agauche_de(B,A).
+
+solve :-
+Maisons = [
+	maison(1, _, _, _, _, _),
+	maison(2, _, _, _, _, _),
+	maison(3, _, _, _, _, _),
+	maison(4, _, _, _, _, _),
+	maison(5, _, _, _, _, _),
+],
+
+member(maison(3, anglais, _, _, rouge, _) ,Maisons).
+
+/*
+member(maison(_, suedois, _, _, chiens, _) ,Maisons).
+member(maison(_, danois, _, the, _, _) ,Maisons).
+
+member(maison(_, anglais, _, _, rouge, _) ,Maisons).
+member(maison(_, anglais, _, _, rouge, _) ,Maisons).
+member(maison(_, anglais, _, _, rouge, _) ,Maisons).
+member(maison(_, anglais, _, _, rouge, _) ,Maisons).
 
 maison(_, anglais, _, _, _, rouge).
 maison(_, suedois, _, _, chiens, _).
@@ -19,3 +37,4 @@ maison(_, _, bluemaster, biere, _, _).
 maison(_, allemand, princes, _, _, _).
 
 maison(A, _, _, _, chats, _) :- maison([A+1,A-1], _, blend, _, _, _).
+*/
